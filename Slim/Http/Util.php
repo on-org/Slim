d31e9809ca67dca612e4347de5ef16f2c4e8f45c
Alexander Kotov <kotov.sanek@gmail.com>
2023-06-23 12:38:54 +0700

    get_magic_quotes_gpc fix

--- Slim/Http/Util.php
+++ Slim/Http/Util.php
@@ -58,7 +58,7 @@ class Util
      */
     public static function stripSlashesIfMagicQuotes($rawData, $overrideStripSlashes = null)
     {
-        $strip = is_null($overrideStripSlashes) ? get_magic_quotes_gpc() : $overrideStripSlashes;
+        $strip = is_null($overrideStripSlashes) ? 0 : $overrideStripSlashes;
         if ($strip) {
             return self::stripSlashes($rawData);
         }
